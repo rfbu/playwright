ac3996e575bd8d846de75d1c2cca4362a653fb58
rfbu <rfbu@users.noreply.github.com>
2024-12-05 12:29:08 +0800

    Add files via upload
    added various functionality: 
    getByRole
    getByText
    locator
    first()
    hover()
    
    for validation:
    tobeVisible()
    tohaveText()
    
    action:
    click()
    hover()

--- e2.spec.ts
+++ e2.spec.ts
@@ -1,15 +1,48 @@
 // e2.spec.ts
 import { test } from './fixtures';
 
-test('example test using page object', async ({ pageObject }) => {
+test('example test using page object', 
+	{tag: ['@first']},
+	async ({ pageObject }) => {
+	
   await pageObject.navigateTo('https://playwright.dev/');
-  let title: string = await pageObject.getTitle();
+  const title: string = await pageObject.getTitle();
   await pageObject.verifyTitle('Playwright enables reliable end-to-end testing for modern web apps.');
   console.log("it is..."+title); // Output the title of the page
 
-  await pageObject.clikLink('Get started');
-  await pageObject.verifyTitle('Installation');
-  title = await pageObject.getTitle();
-  console.log("it is..."+title); // Output the title of the page
+	//await pageObject.verifyLinkVisible('Playwright');
+	await pageObject.verifyLinkVisible('Docs');
+	await pageObject.verifyLinkVisible('API');
+	await pageObject.verifyButtonVisible('Node.js');
+	
+	await pageObject.hoverButton('Node.js');
+	await pageObject.clikLink('Python');
+	await pageObject.verifyTextVisible('Playwright for Python');
 
+	await pageObject.hoverButton('Python');	
+	await pageObject.clikLink('Java');
+	await pageObject.verifyTextVisible('Playwright for Java');
+
+	await pageObject.hoverButton('Java');
+	await pageObject.clikLink('.NET');
+	await pageObject.verifyTextVisible('Playwright for .NET');
+});
+
+test('todo',
+	{tag: ['@todo']}, 
+	async ({ pageObject }) => {
+	
+  await pageObject.navigateTo('https://playwright.dev/');
+  
+  await pageObject.clikLink('Get started');
+
+  await pageObject.verifyTitle('Installation');
+  await pageObject.clikLink('Next Writing tests');
+  await pageObject.verifyTitle('Writing tests');
+
+  await pageObject.clikLink('Next Generating tests');  
+  await pageObject.verifyTitle('Generating tests');
+  
+  await pageObject.clikLink('Next Running and debugging tests');
+  await pageObject.verifyTitle('Running and debugging tests');
 });
--- po.ts
+++ po.ts
@@ -12,19 +12,51 @@ export class PageObject {
 	return await this.page.locator('h1').innerText();
   }
   
-  async verifyTitle(title: string) {
-    await expect(this.page.getByRole('heading',{name: title})).toBeVisible();
+  async verifyLinkVisible(link: string)
+  {
+    await expect(this.page.getByRole('link', { name: link, exact: true  }).first()).toBeVisible();  
   }
-   
-  async clickButton(selector: string) {
-    await this.page.click(selector);
+  
+  async verifyGenericVisible(link: string)
+  {
+    await expect(this.page.getByRole('generic', { name: link, exact: true  }).first()).toBeVisible();  
   }
 
-  async inputText(selector: string, text: string) {
-    await this.page.fill(selector, text);
+  async verifyTextVisible(link: string)
+  {
+    await expect(this.page.getByText(link, {exact: true})).toBeVisible();  
+  }
+
+  async verifyButtonVisible(link: string)
+  {
+    await expect(this.page.getByRole('button', { name: link })).toBeVisible();  
+  }
+  
+  async verifyTitle(title: string) {
+    await expect(this.page.locator('h1')).toHaveText(title);
+  }
+   
+  async clikButton(title: string){
+	await this.page.getByRole('button', { name: title }).click();
+  }
+  
+  async hoverButton(link: string){
+	await this.page.getByRole('button', { name: link }).hover();
   }
   
   async clikLink(link: string){
-	await this.page.getByRole('link', { name: link }).click();
+	await this.page.getByRole('link', { name: link }).first().click();
+  }
+
+  async hoverLink(link: string){
+	await this.page.getByRole('link', { name: link }).hover();
+  }
+  
+  async clikNext(){
+	await this.page.getByText(/Next/).click();
+  }//flaky
+  
+  async inputText(selector: string, text: string) {
+    await this.page.fill(selector, text);
   }
 }
